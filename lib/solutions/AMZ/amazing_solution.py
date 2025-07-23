882e6ceda7666b1a8d214212786dcf3a22f7fd17
sbx_user1051 <sbx_user1051@169.254.10.113>
2025-07-23 19:39:57 +0000

    Wed Jul 23 19:39:57 UTC 2025

--- lib/solutions/AMZ/amazing_solution.py
+++ lib/solutions/AMZ/amazing_solution.py
@@ -3,17 +3,28 @@ import subprocess
 
 
 class AmazingSolution:
-    def __init__(self, app = Path('./amazing.py')):
+    def __init__(self, app = Path("./amazing.py")):
         dir = Path(__file__).parent.absolute()
         self.app = dir / app
 
     def amazing_maze(self, rows, columns, maze_generation_options):
-        with subprocess.Popen(self.app, stdin = subprocess.PIPE, stdout = subprocess.PIPE, encoding = 'utf8') as process:
-            while True:
-                line = process.stdout.readline().rstrip()
+        with subprocess.Popen(
+            self.app,
+            stdin = subprocess.PIPE,
+            stdout = subprocess.PIPE,
+            encoding = "utf8",
+        ) as process:
+            for line in process.stdout:
                 if not line or line.startswith(' '):
                     continue
-                return line
+                match line:
+                    case "WHAT ARE YOUR WIDTH AND LENGTH":
+                        process.stdin.write(f"{columns}\n")
+                        process.stdin.write(f"{rows}\n")
+                        break
+                    case prompt:
+                        raise Exception(f"Unknown prompt: {prompt}")
+
 
 
 
--- test/solution_tests/AMZ/test_amazing_solution.py
+++ test/solution_tests/AMZ/test_amazing_solution.py
@@ -4,26 +4,19 @@ from solutions.AMZ.amazing_solution import AmazingSolution
 class TestSum():
     def test_io(self):
         solution = AmazingSolution()
-        generated_maze = solution.amazing_maze(4, 3, {})
+        generated_maze = solution.amazing_maze(5, 7, {})
         assert generated_maze == """
-.--.--.--.--.  .--.--.--.--.
-I     I     I              I
-:  :  :  :  :  :  :--:--:--.
-I  I  I  I  I  I           I
-:  :  :  :  :  :--:  :--:  .
-I  I  I  I  I     I     I  I
-:  :  :  :  :--:  :--:--:  .
-I  I  I  I     I        I  I
-:  :  :  :--:  :--:--:  :  .
-I  I  I     I  I     I  I  I
-:  :  :--:  :  :  :  :  :  .
-I  I     I  I  I  I  I  I  I
-:  :--:  :  :  :  :  :  :  .
-I  I  I  I  I  I  I  I  I  I
-:  :  :  :  :  :  :  :  :  .
-I  I  I  I  I  I  I  I  I  I
-:  :  :  :  :  :  :  :  :  .
-I     I     I     I     I  I
-:--:--:--:--:--:--:--:--:  .
+.--.--.--.  .--.--.--.
+I        I           I
+:  :  :  :  :  :--:--.
+I  I  I  I  I        I
+:  :  :  :  :--:  :  .
+I  I  I  I     I  I  I
+:  :  :  :--:  :--:  .
+I  I  I     I     I  I
+:  :  :--:  :--:  :  .
+I  I     I        I  I
+:--:--:--:--:--:--:  .
 """.lstrip()
+
 
